c41e76ac59e1cec9226bbf716e795e9360acba27
hasibmiraz <hasibmiraz09@gmail.com>
2022-07-10 00:31:51 +0600

    Create OTP and store in DB

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,7 @@
+const nodemailer = require('nodemailer');
+
 const User = require('../models/user');
+const EmailVerificationToken = require('../models/emailVerificationToken');
 
 exports.create = async (req, res) => {
   const { name, email, password } = req.body;
@@ -10,14 +13,41 @@ exports.create = async (req, res) => {
   const newUser = new User({ name, email, password });
   await newUser.save();
 
-  // var transport = nodemailer.createTransport({
-  //   host: "smtp.mailtrap.io",
-  //   port: 2525,
-  //   auth: {
-  //     user: "49263afb4c4efb",
-  //     pass: "1a13ef8720d243"
-  //   }
-  // });
+  // Generate 6 digit OTP
+  let OTP = '';
+  for (let i = 0; i <= 5; i++) {
+    const randomVal = Math.round(Math.random() * 9);
+    OTP += randomVal;
+  }
+  // Store it in the DB
+  const newEmailVerificationToken = new EmailVerificationToken({
+    owner: newUser._id,
+    token: OTP,
+  });
 
-  res.status(201).json({ user: newUser });
+  await newEmailVerificationToken.save();
+  // Send OTP to the user
+  var transport = nodemailer.createTransport({
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+
+  transport.sendMail({
+    from: 'miraz@movie-review-app.com',
+    to: newUser.email,
+    subject: 'Verify your email',
+
+    html: `
+    <p>Your verification OTP</p>
+    <h1>${OTP}</h1>    
+    `,
+  });
+
+  res.status(201).json({
+    message: 'Email has been sent to your email. Please verify your account!',
+  });
 };
--- middlewares/validator.js
+++ middlewares/validator.js
@@ -5,6 +5,7 @@ exports.userValidator = [
   check('email').normalizeEmail().isEmail().withMessage('Email is invalid!'),
   check('password')
     .trim()
+    .not()
     .isEmpty()
     .withMessage('Password is missing!')
     .isLength({ min: 8, max: 20 })
@@ -14,8 +15,8 @@ exports.userValidator = [
 exports.validate = (req, res, next) => {
   const error = validationResult(req).array();
   if (error.length) {
+    console.log(error);
     return res.json({ error: error[0].msg });
   }
-  console.log(error);
   next();
 };
