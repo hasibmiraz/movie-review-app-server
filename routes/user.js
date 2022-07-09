f92058a76f3abb53711bb9841d9fe622a3578d8d
hasibmiraz <hasibmiraz09@gmail.com>
2022-07-09 18:41:36 +0600

    Validation added with express validator

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { create } = require('../controllers/user');
+const { userValidator, validate } = require('../middlewares/validator');
 
-router.post('/create', create);
+router.post('/create', userValidator, validate, create);
 
 module.exports = router;
