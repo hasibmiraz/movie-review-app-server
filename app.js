2e79103276228fb62ebfdc9529ecd92efdd1deb4
hasibmiraz <hasibmiraz09@gmail.com>
2022-07-11 22:54:20 +0600

    Error handling completed

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 const express = require('express');
+const morgan = require('morgan');
+require('express-async-errors');
 require('dotenv').config();
 
 require('./db');
 const userRouter = require('./routes/user');
+const { errorHandler } = require('./middlewares/errorHandler');
 
 const app = express();
 app.use(express.json());
+app.use(morgan('dev'));
 const port = process.env.PORT || 5000;
 
 // Routes
 app.use('/api/user', userRouter);
+
+// error handling middleware
+app.use(errorHandler);
 
 // Port
 app.listen(port, () => {
